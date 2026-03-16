0721fa014961eb3e67e5e85374fd592099f52894
Chris Rohlfs <seeken@gmail.com>
2026-03-15 22:02:39 -0600

    Add postgres rollup SQL coverage

--- test/selecto_db_postgresql/adapter_test.exs
+++ test/selecto_db_postgresql/adapter_test.exs
@@ -1,6 +1,12 @@
 defmodule SelectoDBPostgreSQL.AdapterTest do
   use ExUnit.Case, async: true
 
+  defmodule Pg18MockRepo do
+    def query("show server_version_num", []) do
+      {:ok, %{rows: [["180001"]]}}
+    end
+  end
+
   test "adapter exposes the selecto adapter contract" do
     assert Code.ensure_loaded?(SelectoDBPostgreSQL.Adapter)
     assert function_exported?(SelectoDBPostgreSQL.Adapter, :name, 0)
@@ -34,6 +40,39 @@ defmodule SelectoDBPostgreSQL.AdapterTest do
     assert SelectoDBPostgreSQL.Adapter.supports?(:rollup)
   end
 
+  test "postgres rollup uses compatibility wrapper by default" do
+    selecto =
+      sales_domain()
+      |> Selecto.configure(:mock_connection,
+        adapter: SelectoDBPostgreSQL.Adapter,
+        validate: false
+      )
+      |> Selecto.select([{:sum, "amount"}])
+      |> Selecto.group_by(rollup: ["region"])
+      |> Selecto.order_by([{"region", :asc}])
+
+    {sql, _aliases, _params} = Selecto.gen_sql(selecto, [])
+
+    assert String.contains?(sql, "rollup")
+    assert String.contains?(sql, "select * from (")
+    assert String.contains?(sql, ") as rollupfix")
+  end
+
+  test "postgres 18 disables rollup compatibility wrapper" do
+    selecto =
+      sales_domain()
+      |> Selecto.configure(Pg18MockRepo, adapter: SelectoDBPostgreSQL.Adapter, validate: false)
+      |> Selecto.select([{:sum, "amount"}])
+      |> Selecto.group_by(rollup: ["region"])
+      |> Selecto.order_by([{"region", :asc}])
+
+    {sql, _aliases, _params} = Selecto.gen_sql(selecto, [])
+
+    assert String.contains?(sql, "rollup")
+    refute String.contains?(sql, "select * from (")
+    refute String.contains?(sql, ") as rollupfix")
+  end
+
   test "postgres adapter validates stream pool references" do
     assert {:error, {:invalid_stream_pool, %{stream_context: :pool}}} =
              SelectoDBPostgreSQL.Adapter.stream({:pool, %{}}, "select 1", [], [])
@@ -50,5 +89,25 @@ defmodule SelectoDBPostgreSQL.AdapterTest do
 
     assert %{type: :unknown, status: :invalid} =
              SelectoDBPostgreSQL.Adapter.connection_info(123)
+  end
+
+  defp sales_domain do
+    %{
+      source: %{
+        source_table: "sales",
+        primary_key: :id,
+        fields: [:id, :region, :amount],
+        redact_fields: [],
+        columns: %{
+          id: %{type: :integer},
+          region: %{type: :string},
+          amount: %{type: :decimal}
+        },
+        associations: %{}
+      },
+      schemas: %{},
+      joins: %{},
+      name: "Sales"
+    }
   end
 end
